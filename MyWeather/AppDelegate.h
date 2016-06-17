265c3b8974bc8d535757941c03dbe96a2d686d31
mwcorrell <mwcorrell@yahoo.com>
2016-06-16 20:11:32 -0400

    Initial Commit

---
+++ MyWeather/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyWeather
+//
+//  Created by Mike Correll on 6/16/16.
+//  Copyright © 2016 Mike Correll. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
